5582b725bf87b3fbf6ef42d49157fd6d25d1ab73
DaniAlBalhaa <dani.al-balhaa.te16@sodertalje.ntig.se>
2019-03-21 12:52:59 +0100

    Inventory Code, Current EP: 07

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,8 +3,16 @@
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class Item : ScriptableObject {
 
-    new public string name = "New Item";
-    public Sprite icon = null;
-    public bool isDefaultItem = false;
+    new public string name = "New Item"; //Name 
+    public Sprite icon = null; //Icon
+    public bool isDefaultItem = false; //Default Wear?
+
+    public virtual void Use ()
+    {
+        //Use the item
+        //Something might happen
+
+        Debug.Log("Using " + name);
+    }
 
 }
